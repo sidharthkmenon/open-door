4eb639bcdadfcd33d788bf2d9fe908b9c583b9e0
Sidharth Menon <sidharthmenon@Sidharths-MBP.columbus.rr.com>
2018-07-14 22:50:26 -0400

    neat

---
+++ Desktop/Summer 2018/Code/Capability_Test.py
@@ -0,0 +1,9 @@
+# %%
+print('Hello World')
+
+# alpha product launch
+print('Quantum AI Blockchain')
+
+# step aside, Google
+for index, letter in enumerate('1 Trillion Dollar Series A Valuation'):
+    print((letter, index))
