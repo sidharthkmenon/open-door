348d5e41424946b4fcc8a9fa6ea46d0b722103d6
Sidharth Menon <sidharthmenon@Sidharths-MacBook-Pro.local>
2018-07-17 17:13:07 -0400

    Tensorflow working

--- Desktop/Summer 2018/Code/Capability_Test.py
+++ Desktop/Summer 2018/Code/Capability_Test.py
@@ -4,6 +4,9 @@ print('Hello World')
 # alpha product launch
 print('Quantum AI Blockchain')
 
+# test
+1 + 2
+
 # step aside, Google
 for index, letter in enumerate('1 Trillion Dollar Series A Valuation'):
     print((letter, index))
